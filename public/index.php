40c184a24ad6f63d99d4caa28755c39006551bfc
Pedro-Piedade <ppgames113chave@gmail.com>
2022-12-16 18:14:07 -0200

    Aula 2

--- public/index.php
+++ public/index.php
@@ -1,18 +1,14 @@
 <?php
 
-include "./teste.php";
+
+$name = "Pedro";
+
+$myName = &$name;
+
+$name = "João";
+
+echo $name;
+echo $myName;
+
 
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  <h2>Olá <?php echo $name ?></h2>
-</body>
-</html>
