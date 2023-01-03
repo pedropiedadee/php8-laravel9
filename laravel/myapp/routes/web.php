94498f9d4d822a12dc06ecafa39edf9829ab22ad
Pedro-Piedade <ppgames113chave@gmail.com>
2023-01-03 20:07:40 -0200

    aula 4 concluida

--- laravel/myapp/routes/web.php
+++ laravel/myapp/routes/web.php
@@ -1,18 +1,26 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('user/{user}', function(User $user) {
+    return $user ;
+});
 
-Route::get('/hello-world', function () {
-    return 'oi';    
+Route::prefix('usuarios')->group(function() {
+    Route::get('/edit', function() {
+        return 'Pedro';
+    })->name('usuarios');
+
+    Route::get('/{id}/tags', function($id) {
+        return 'tags ' . $id;
+    })->name('tags');
+});
+
+Route::get('/a-empresa/{string?}', function ($string = null) {
+    return $string;
+})->name('a-empresa');;
+
+Route::get('/users/{paramA}/{paramB}', function ($paramA, $paramB) {
+    return $paramA . ' - ' . $paramB;    
 });
