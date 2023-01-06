416ba004182bbc342c6ee8f349d31db8f4dc4c3c
Pedro-Piedade <ppgames113chave@gmail.com>
2023-01-05 22:47:39 -0200

    Controllers

--- laravel/myapp/routes/web.php
+++ laravel/myapp/routes/web.php
@@ -1,26 +1,18 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('user/{user}', function(User $user) {
-    return $user ;
+Route::get('/redirect', function(Request $request) {
+    $r = $request->input();
+    dd($r);
+    return 'x';
 });
 
-Route::prefix('usuarios')->group(function() {
-    Route::get('/edit', function() {
-        return 'Pedro';
-    })->name('usuarios');
+Route::get('user/{user}', [UserController::class, 'show']);
 
-    Route::get('/{id}/tags', function($id) {
-        return 'tags ' . $id;
-    })->name('tags');
-});
-
-Route::get('/a-empresa/{string?}', function ($string = null) {
-    return $string;
-})->name('a-empresa');;
-
-Route::get('/users/{paramA}/{paramB}', function ($paramA, $paramB) {
-    return $paramA . ' - ' . $paramB;    
+Route::get('/', function () {
+    return view('welcome');
 });
