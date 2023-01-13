f2c1fe23f9598a2d42839b6e9f040db266f701ae
Pedro-Piedade <ppgames113chave@gmail.com>
2023-01-13 18:54:38 -0200

    Update com eloquent

--- laravel/myapp/app/Http/Controllers/BusinessController.php
+++ laravel/myapp/app/Http/Controllers/BusinessController.php
@@ -9,13 +9,39 @@ use App\Models\User;
 class BusinessController extends Controller
 {
     public function index()
-    {
-        $business = Business::create([
-            'name' => 'Joh Snow',
-            'email' => 'johsnow@gmail.com',
-            'address' => 'Rua A Quadra B',
-        ]);
+    {          
+        //UMA FORMA DE DAR UPDATE
+        // $business = Business::find(3);
 
+        //$business->name = 'Pedro Piedade';
+        //$business->email = 'johsnow@gmail.com',;
+        //$business->address = 'Rua Portugal';
+        //$business->save();
+
+        //dd($business);
+
+        
+        //OUTRA FORMA
+        // $business = Business::find(3)
+        //   ->update([
+        //     'name' => 'Joh Snow',
+        //     'email' => 'johsnow@gmail.com',
+        //     'address' => 'Rua do Joh',
+        //   ]);
+        // dd($business);
+
+
+        //MAIS UMA FORMA (MAIS UTILIZADA)
+        $input = [
+            'name' => 'Pedro Piedade',
+            'email' => 'laravel@gmail.com',
+            'address' => 'Rua Portugal',
+        ];
+
+        $business = Business::find(3);
+        $business->fill($input);
+        $business->save();
         dd($business);
+
     }
 }
