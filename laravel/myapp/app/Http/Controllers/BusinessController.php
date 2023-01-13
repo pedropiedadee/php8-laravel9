446aa39d7140901d31119ed92cb27ebb605a8bcc
Pedro-Piedade <ppgames113chave@gmail.com>
2023-01-13 18:37:19 -0200

    salvando registros no banco de dados(Eloquent)

--- laravel/myapp/app/Http/Controllers/BusinessController.php
+++ laravel/myapp/app/Http/Controllers/BusinessController.php
@@ -10,22 +10,12 @@ class BusinessController extends Controller
 {
     public function index()
     {
-        //PEGAR TODOS OS REGISTROS:
-        $businesses = Business::all();
-        //dd($businesses);
+        $business = Business::create([
+            'name' => 'Joh Snow',
+            'email' => 'johsnow@gmail.com',
+            'address' => 'Rua A Quadra B',
+        ]);
 
-        //ENCONTRAR UM ÚNICO REGISTRO:
-        $business = Business::find(1);       
-
-        //PEGAR UM REGISTRO COM WHERE(ONDE) OBS: VEM UMA COLEÇÃO:
-        $businessWhere = Business::where('name', 'Boyle Ltd')->get();
-
-        //PARA VIR UM ITEM SÓ (SEM COLEÇÃO):
-        $businessWhereFirst = Business::where('name', 'Boyle Ltd')->first();
-        dd($businessWhereFirst, $businessWhere ,$business, $businesses);
-
-        //PEGANDO USER COM O ID 1
-        $user1 = User::find(1);
-        dd($user1);
+        dd($business);
     }
 }
